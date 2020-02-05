6c01b98ab85eea16f3cd7a082fffd0c44b79af86
xiaohai <xiaohai@anve.com>
2020-02-05 14:49:14 +0800

    page

---
+++ src/components/login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="header">
+      11111111
+
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import marquee from '@/components/commons/marquee'
+import constants from '@/js/constants'
+import * as types from '@/store/mutation-types'
+export default {
+    components: {
+      marquee
+    },
+    data() {
+        return {
+          
+        }
+    },
+    
+    mounted() {
+      
+     
+      
+    },
+    methods: {
+      
+    },
+    beforeDestroy() {
+      eBus.unregister(constants.HEADER)
+  	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
